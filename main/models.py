a58a41b952f6dbd8d858215da919f7d59163c8d7
Jordan Hyatt <jordan.t.hyatt@gmail.com>
2022-03-26 12:10:30 -0400

    bulding up data model and methods for reading in raw data

--- main/models.py
+++ main/models.py
@@ -98,6 +98,9 @@ class SeasonDay(models.Model):
     season = models.ForeignKey('Season', on_delete=models.CASCADE)
     day_num = models.IntegerField()
 
+    class Meta:
+        unique_together = ['season', 'day_num']
+
     def __str__(self):
         return f'{self.season} | {self.day_num}'
 
@@ -115,6 +118,9 @@ class TeamCoach(models.Model):
     season = models.ForeignKey('Season', on_delete=models.CASCADE)
     first_day = models.ForeignKey('SeasonDay', on_delete=models.CASCADE, related_name='first_day_set')
     last_day = models.ForeignKey('SeasonDay', on_delete=models.CASCADE, related_name='last_day_set')
+
+    class Meta:
+        unique_together = ['team', 'coach', 'first_day']
 
     @classmethod
     def update_objs(cls):
@@ -146,6 +152,9 @@ class TeamConference(models.Model):
     season = models.ForeignKey('Season', on_delete=models.CASCADE)
     conference = models.ForeignKey('Conference', on_delete=models.CASCADE)
 
+    class Meta:
+        unique_together = ['team', 'season']
+
     @classmethod
     def update_objs(cls):
         ''' A method to update conferences from csv '''
@@ -171,6 +180,9 @@ class TeamRank(models.Model):
     system = models.CharField(max_length=20)
     rank = models.IntegerField()
 
+    class Meta:
+        unique_together = ['team', 'season', 'ranking_day', 'system']
+
     @classmethod
     def update_objs(cls):
         ''' A method to update team rankings from csv '''
@@ -195,12 +207,24 @@ class Game(models.Model):
     GT_CHOICES = (('Regular','Regular'),('NCAA','NCAA'),('Secondary','Secondary'))
     season = models.ForeignKey('Season', on_delete=models.CASCADE)
     day = models.ForeignKey('SeasonDay', on_delete=models.CASCADE)
-    city = models.ForeignKey('City', on_delete=models.CASCADE)
+    city = models.ForeignKey('City', on_delete=models.CASCADE, null=True)
     gid = models.CharField(max_length=50)
 
     teams = models.ManyToManyField('Team')
     num_ot = models.IntegerField(null=True)
     game_type = models.CharField(choices=GT_CHOICES, max_length=10, null=True)
+
+    class Meta:
+        unique_together = ['day', 'gid']
+
+
+    @property
+    def has_season(self):
+        try:
+            return bool(self.season)
+        except Season.DoesNotExist:
+            return False
+
 
     @classmethod
     def update_objs(cls):
@@ -215,11 +239,30 @@ class Game(models.Model):
             t1 = Team.objects.filter(kid=tup.WTeamID).first()
             t2 = Team.objects.filter(kid=tup.LTeamID).first()
             obj, _ = cls.objects.update_or_create(
-                season=season, day=day, city=city, gid=gid,
-                defaults=dict(season=season, game_type=tup.CRType)
+                day=day, gid=gid,
+                defaults=dict(season=season, game_type=tup.CRType, city=city)
             )
             obj.teams.add(*[t1,t2])
             
+    def get_season(self):
+        ''' A method to derive the season from the day '''
+        if self.day and not self.has_season:
+            self.season = self.day.season
+
+    def get_teams(self):
+        ''' A method to derive teams from gid '''
+        kid1 = self.gid.split('_')[0]
+        kid2 = self.gid.split('_')[1]
+        team1 = Team.objects.filter(kid=kid1).first()
+        team2 = Team.objects.filter(kid=kid2).first()
+        self.teams.add(*[team1, team2])
+
+
+    def save(self, *args, **kwargs):
+        self.get_season()
+        super().save(*args, **kwargs)
+        self.get_teams()
+
     def __str__(self):
         return f'{self.day} | {self.teams.all()}'
 
@@ -231,20 +274,84 @@ class GameTeam(models.Model):
     game = models.ForeignKey('Game', on_delete=models.CASCADE)
     team = models.ForeignKey('Team', on_delete=models.CASCADE, related_name='gameteam_set')
     opponent = models.ForeignKey('Team', on_delete=models.CASCADE, related_name='gameteam_opp_set')
-
     loc = models.CharField(choices=LOC_CHOICES, max_length=10)
     result = models.CharField(choices=RESULT_CHOICES, max_length=10)
 
-    def __str__(self):
-        return f'{self.game} | {self.team}'
+    class Meta:
+        unique_together = ['game', 'team']
 
-    @static
+    @staticmethod
+    def df_generator():
+        files = ['MRegularSeasonDetailedResults.csv', 'MNCAATourneyDetailedResults.csv']
+        for fn in files:
+            path = os.path.join(settings.BASE_DATA_PATH, fn)
+            yield pd.read_csv(path)
 
     @classmethod
     def update_objs(cls):
-        files = ['MRegularSeasonDetailedResults.csv']
+        for df in cls.df_generator():
+            cls.process_df(df)
         
+    @classmethod
+    def process_df(cls, df):
+        ''' Class method to update objs from df produced by Kaggle csv '''
+        for tup in tqdm(df.itertuples()):
+            season = Season.objects.get(year=tup.Season)
+            day, _ = SeasonDay.objects.get_or_create(season=season, day_num=tup.DayNum)
+            gid = f'{tup.WTeamID}_{tup.LTeamID}'
+            game, _ = Game.objects.update_or_create(
+                day=day, gid=gid,
+                defaults=dict(num_ot=tup.NumOT)
+            )
 
+            wteam = Team.objects.get(kid=tup.WTeamID)
+            lteam = Team.objects.get(kid=tup.LTeamID)
+
+            wloc = tup.WLoc
+            if wloc == 'H':
+                lloc = 'A'
+            elif wloc == 'A':
+                lloc = 'H'
+            else:
+                lloc = 'N'
+
+            wgt, _ = GameTeam.objects.update_or_create(
+                game=game, team=wteam,
+                defaults=dict(
+                    loc=wloc, opponent=lteam, result='win'
+                )
+            )
+            lgt, _ = GameTeam.objects.update_or_create(
+                game=game, team=lteam,
+                defaults=dict(
+                    loc=lloc, opponent=wteam, result='loss'
+                )
+            )
+
+            for gs in GameStat.objects.all():
+                wval = getattr(tup, f'W{gs.code}')
+                lval = getattr(tup, f'L{gs.code}')
+
+                wgts = GameTeamStat.objects.update_or_create(
+                    gameteam=wgt, stat=gs, opp_stat=False,
+                    defaults=dict(value=wval)
+                )
+                wgtso = GameTeamStat.objects.update_or_create(
+                    gameteam=wgt, stat=gs, opp_stat=True,
+                    defaults=dict(value=lval)
+                )
+
+                lgts = GameTeamStat.objects.update_or_create(
+                    gameteam=lgt, stat=gs, opp_stat=False,
+                    defaults=dict(value=lval)
+                )
+                lgtso = GameTeamStat.objects.update_or_create(
+                    gameteam=lgt, stat=gs, opp_stat=True,
+                    defaults=dict(value=wval)
+                )
+
+    def __str__(self):
+        return f'{self.game} | {self.team}'
 
 class GameStat(models.Model):
     ''' Represents a stat that can be given a value for any particular game '''
@@ -263,3 +370,14 @@ class GameStat(models.Model):
 
     def __str__(self):
         return f'{self.code} | {self.desc}'
+
+
+class GameTeamStat(models.Model):
+    ''' Represents a stat value for a given GameTeam '''
+    class Meta:
+        unique_together = ['gameteam', 'stat', 'opp_stat']
+    
+    gameteam = models.ForeignKey('GameTeam', on_delete=models.CASCADE)
+    stat = models.ForeignKey('GameStat', on_delete=models.CASCADE)
+    opp_stat = models.BooleanField(verbose_name='Opponent Stat Flag')
+    value = models.FloatField()
---
+++ scripts/init_db.py
@@ -0,0 +1,14 @@
+from main.models import *
+
+
+def run(*args):
+    City.update_objs()
+    Conference.update_objs()
+    Team.update_objs()
+    Season.update_objs()
+    GameStat.update_objs()
+    TeamCoach.update_objs()
+    TeamConference.update_objs()
+    TeamRank.update_objs()
+    Game.update_objs()
+    GameTeam.update_objs()
